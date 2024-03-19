9c952e601461e8f6ae3c38b68e16fa040680687c
Rodrigo Oliveira <rodrigo_oliveira@live.com>
2024-03-19 16:22:30 -0300

    Updated route name

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8000;
 
 app.use(express.json());
 app.use(cors({}));
-app.use('/books', routeBooks);
+app.use('/livros', routeBooks);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
